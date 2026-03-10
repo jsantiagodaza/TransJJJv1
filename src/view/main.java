c29c9e16e80f10a1340bdaadc0b1f463900e5bac
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-10 18:32:29 -0500

    Se hizo la función menuPrincipal()

--- src/view/main.java
+++ src/view/main.java
@@ -10,6 +10,20 @@ Scanner sc new Scanner(System.in); //<--usaremos scanner para leer, no usaremos 
 
 
     }
+    static int menuPrincipal(){
+        int opcion = 0;
+    System.out.println("\n-----------------------------------------------");
+    System.out.println("|           SISTEMAS DE BUSES              |");
+    System.out.println("|------------------------------------------------|");
+    System.out.println("| 1.  Gestión de pasajeros                      |");
+    System.out.println("| 2. Gestión de Vehículos                       |");
+    System.out.println("| 3. Gestión de ticket                               |");
+    System.out.println("| 4. salir                                                    |");
+    System.out.println("|------------------------------------------------|");
+    System.out.println(" ");
+    System.out.println("seleccione(1-4):");
+    return (opcion);
+}
 }
     
 
