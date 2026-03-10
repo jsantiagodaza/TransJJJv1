666c69bf364da08da6ce7fbf322726261c6c50ba
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-10 17:44:15 -0500

    feat: se añadieron los atributos correspondientes a la clase ticket.java

--- src/clases/ticket.java
+++ src/clases/ticket.java
@@ -1,5 +1,18 @@
 
 package clases;
+
+import java.time.LocalDate; //<--para que usemos la fecha
+import java.time.LocalTime; //<--para que usemos la hora
+
 public class ticket {
+    private String Id; //<--esta tiene que ser única e irrepetible
+    private String Empresa; //<--El nombre de la empresa que proprciona el ticket
+    private String CodigoTicket; //<--este puede ser la concatenación que mejor nos parezca, propongo "BUS" y un número de X digitos
+    //ejemplo "BUS-3984" o si es micro bus "MBUS-1933" por ejemplo
+    private LocalDate fechaviaje;
+    private LocalTime horaviaje;
+    private String Idvehiculo;
+    private float precioticket;
+    
     
 }
