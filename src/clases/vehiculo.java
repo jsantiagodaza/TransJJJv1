825fd480af1c1a3c080c682ce2b302b1d2813d60
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-09 21:07:59 -0500

    feat: se agregarón los atributos de la clase vehiculo

--- src/clases/vehiculo.java
+++ src/clases/vehiculo.java
@@ -1,5 +1,42 @@
 
 package clases;
 public class vehiculo {
+    private String placa;
+    private String marca;
+    private String modelo;
+    private String tipo;
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
     
 }
