1b62b9bb5882fba352e276b73d82a31e6349bc03
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-09 21:19:29 -0500

    feat: se hicieron los getters y setters de Persona

--- src/clases/persona.java
+++ src/clases/persona.java
@@ -6,4 +6,15 @@ public class persona {
     private String cedula;
     private String telefono;
 
+    public persona(String id, String nombre, String cedula, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.telefono = telefono;
+    }
+
+    public persona() {
+    }
+
+    
 }
