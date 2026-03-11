393517c5191fef95c8db12a047bc0f65dd4cd1c2
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-10 19:33:57 -0500

    refactor: se le aplico formato al código completo de la clase main

--- src/view/main.java
+++ src/view/main.java
@@ -3,79 +3,82 @@ package view;
 import clases.persona;
 import java.util.ArrayList;
 import java.util.Scanner;
-public class main {
-    static ArrayList<persona> personas = new ArrayList<>();//array para personas
-    public static void main(String[] args) {
-   
-Scanner Sc = new Scanner(System.in); //<--usaremos scanner para leer, no usaremos bufered reader
 
+public class main {
+
+    static ArrayList<persona> personas = new ArrayList<>();//array para personas
+
+    public static void main(String[] args) {
+
+        Scanner Sc = new Scanner(System.in); //<--usaremos scanner para leer, no usaremos bufered reader
 
     }
-    static int menuPrincipal(){
+
+    static int menuPrincipal() {
         Scanner Sc = new Scanner(System.in);
         int opcion = 0;
-    System.out.println("\n-----------------------------------------------");
-    System.out.println("|           SISTEMAS DE BUSES              |");
-    System.out.println("|------------------------------------------------|");
-    System.out.println("| 1.  Gestión de pasajeros                      |");
-    System.out.println("| 2. Gestión de Vehículos                       |");
-    System.out.println("| 3. Gestión de ticket                               |");
-    System.out.println("| 4. salir                                                    |");
-    System.out.println("|------------------------------------------------|");
-    System.out.println(" ");
-    System.out.println("seleccione(1-4):");
-    opcion = Sc.nextInt();
-    return (opcion);
-}
-  static int menuPersonas(){
-      Scanner Sc = new Scanner(System.in);
-  int opcionpasajero = 0;
-  System.out.println("\n--------------------------------------------------");
-    System.out.println("|     SISTEMAS DE PASAJEROS          |");
-    System.out.println("|------------------------------------------------|");
-    System.out.println("| 1.  Registrar de pasajeros                   |");
-    System.out.println("| 2. Listar Pasajeros                               |");
-    System.out.println("| 3. Buscar por cédula                            |");
-    System.out.println("| 4. salir                                                    |");
-    System.out.println("|------------------------------------------------|");
-    System.out.println(" ");
-    System.out.println("seleccione(1-4):");
-    opcionpasajero = Sc.nextInt();
-    return (opcionpasajero);
-  }  
-  static int menuVehiculos(){
-      Scanner Sc = new Scanner(System.in);
-  int opcionvehiculos= 0;
-  System.out.println("\n--------------------------------------------------");
-    System.out.println("|                     VEHICULOS                     |");
-    System.out.println("|------------------------------------------------|");
-    System.out.println("| 1.  Registrar de vehículos                    |");
-    System.out.println("| 2. Listar Vehículos                                |");
-    System.out.println("| 3. Buscar por placa                              |");
-    System.out.println("| 4. salir                                                    |");
-    System.out.println("|------------------------------------------------|");
-    System.out.println(" ");
-    System.out.println("seleccione(1-4):");
-    opcionvehiculos = Sc.nextInt();
-    return (opcionvehiculos);
-  }  
-  static int menuTickets(){
-      Scanner Sc = new Scanner(System.in);
-  int opcionTicket= 0;
-  System.out.println("\n--------------------------------------------------");
-    System.out.println("|                         TICKETS                       |");
-    System.out.println("|------------------------------------------------|");
-    System.out.println("| 1. Crear Ticket                                      |");
-    System.out.println("| 2. Listar Tickets                                    |");
-    System.out.println("| 3. Buscar por código                            |");
-    System.out.println("| 4. salir                                                    |");
-    System.out.println("|------------------------------------------------|");
-    System.out.println(" ");
-    System.out.println("seleccione(1-4):");
-    opcionTicket = Sc.nextInt();
-    return (opcionTicket);
-  }  
-}
-    
+        System.out.println("\n-----------------------------------------------");
+        System.out.println("|           SISTEMAS DE BUSES              |");
+        System.out.println("|------------------------------------------------|");
+        System.out.println("| 1.  Gestión de pasajeros                      |");
+        System.out.println("| 2. Gestión de Vehículos                       |");
+        System.out.println("| 3. Gestión de ticket                               |");
+        System.out.println("| 4. salir                                                    |");
+        System.out.println("|------------------------------------------------|");
+        System.out.println(" ");
+        System.out.println("seleccione(1-4):");
+        opcion = Sc.nextInt();
+        return (opcion);
+    }
 
+    static int menuPersonas() {
+        Scanner Sc = new Scanner(System.in);
+        int opcionpasajero = 0;
+        System.out.println("\n--------------------------------------------------");
+        System.out.println("|     SISTEMAS DE PASAJEROS          |");
+        System.out.println("|------------------------------------------------|");
+        System.out.println("| 1.  Registrar de pasajeros                   |");
+        System.out.println("| 2. Listar Pasajeros                               |");
+        System.out.println("| 3. Buscar por cédula                            |");
+        System.out.println("| 4. salir                                                    |");
+        System.out.println("|------------------------------------------------|");
+        System.out.println(" ");
+        System.out.println("seleccione(1-4):");
+        opcionpasajero = Sc.nextInt();
+        return (opcionpasajero);
+    }
 
+    static int menuVehiculos() {
+        Scanner Sc = new Scanner(System.in);
+        int opcionvehiculos = 0;
+        System.out.println("\n--------------------------------------------------");
+        System.out.println("|                     VEHICULOS                     |");
+        System.out.println("|------------------------------------------------|");
+        System.out.println("| 1.  Registrar de vehículos                    |");
+        System.out.println("| 2. Listar Vehículos                                |");
+        System.out.println("| 3. Buscar por placa                              |");
+        System.out.println("| 4. salir                                                    |");
+        System.out.println("|------------------------------------------------|");
+        System.out.println(" ");
+        System.out.println("seleccione(1-4):");
+        opcionvehiculos = Sc.nextInt();
+        return (opcionvehiculos);
+    }
+
+    static int menuTickets() {
+        Scanner Sc = new Scanner(System.in);
+        int opcionTicket = 0;
+        System.out.println("\n--------------------------------------------------");
+        System.out.println("|                         TICKETS                       |");
+        System.out.println("|------------------------------------------------|");
+        System.out.println("| 1. Crear Ticket                                      |");
+        System.out.println("| 2. Listar Tickets                                    |");
+        System.out.println("| 3. Buscar por código                            |");
+        System.out.println("| 4. salir                                                    |");
+        System.out.println("|------------------------------------------------|");
+        System.out.println(" ");
+        System.out.println("seleccione(1-4):");
+        opcionTicket = Sc.nextInt();
+        return (opcionTicket);
+    }
+}
