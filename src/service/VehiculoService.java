242dc4b16dbdc6c3bee92503b7b1683ce6f23816
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-11 22:34:01 -0500

    feat:creacion del metodo modificar

--- src/service/VehiculoService.java
+++ src/service/VehiculoService.java
@@ -22,4 +22,67 @@ public class VehiculoService {
         this.vehiculos = new ArrayList<>(dao.cargarVehiculos()); // carga al inicio
     }
     
+      public boolean registrarVehiculo(Vehiculo v) {
+        if (existePlaca(v.getPlaca())) {
+            System.out.println(" Ya existe un vehiculo con la placa: " + v.getPlaca());
+            return false;
+        }
+        vehiculos.add(v);
+        dao.guardarVehiculo(v);
+        System.out.println(" Vehiculo registrado exitosamente.");
+        return true;
+    }
+      
+         public boolean cambiarEstado(String placa, boolean nuevoEstado) {
+        Vehiculo v = buscarPorPlaca(placa);
+        if (v == null) {
+            System.out.println(" Vehiculo no encontrado.");
+            return false;
+        }
+        v.setDisponible(nuevoEstado);
+        dao.reescribirArchivo(vehiculos);
+        System.out.println(" Estado actualizado: " + (nuevoEstado ? "Disponible" : "No disponible"));
+        return true;
+    }
+         
+         public Vehiculo buscarPorPlaca(String placa) {
+        for (Vehiculo v : vehiculos) {
+            if (v.getPlaca().equalsIgnoreCase(placa)) {
+                return v;
+            }
+        }
+        return null;
+    }
+
+    // Listar todos
+    public List<Vehiculo> listarVehiculos() {
+        return vehiculos;
+    }
+
+    // Solo los disponibles con cupos
+    public List<Vehiculo> getVehiculosDisponibles() {
+        List<Vehiculo> disponibles = new ArrayList<>();
+        for (Vehiculo v : vehiculos) {
+            if (v.isDisponible() && v.tieneCupos()) {
+                disponibles.add(v);
+            }
+        }
+        return disponibles;
+    }
+
+    // Sumar un pasajero al vehículo (lo usa TicketService del Dev 2)
+    public boolean agregarPasajero(String placa) {
+        Vehiculo v = buscarPorPlaca(placa);
+        if (v == null || !v.tieneCupos()) return false;
+        v.agregarPasajero();
+        dao.reescribirArchivo(vehiculos);
+        return true;
+    }
+
+    private boolean existePlaca(String placa) {
+        return buscarPorPlaca(placa) != null;
+    }
+         
+         
+    
 }
