008882d989864d85075ce95370e1459f13b8addf
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-11 21:00:57 -0500

    feat:cracion de vehiculo y su imprimible

---
+++ src/model/Vehiculo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author 2jcue
+ */
+public class Vehiculo {
+  
+
+    protected String placa;
+    protected String ruta;
+    protected int capacidadMaxima;
+    protected int pasajerosActuales;
+    protected boolean disponible;
+}
